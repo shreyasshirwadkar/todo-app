eafe8ad8b3f9f7998f96e0ac37e7a237fff1d1d2
shreyasshirwadkar <shreyas.shirwadkar1@gmail.com>
2025-01-05 22:26:19 +0530

    Frontend Done

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,8 @@
+export function CreateTodo(){
+    return <div>
+        <input style={{padding:10, margin:10}} type="text" placeholder="title"></input><br/><br/>
+        <input style={{padding:10, margin:10}} type="text" placeholder="description"></input> <br/> <br/>
+        <button style={{padding:10, margin:10}}>Add todo</button>
+
+    </div>
+}
